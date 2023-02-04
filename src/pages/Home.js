35d8eef4b2025db20cde939caa5487353da9aef3
Ethan Ogle <encogle@gmail.com>
2023-02-04 17:44:21 -0500

    dsajg

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import "../App.scss";
-import Slider from "../components/slider/Slider";
 
 const Home = () => {
   return (
     
     <div>
-      Hello
+      
     </div>
   )
 }
