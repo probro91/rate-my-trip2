91c8626ba42d85dfee33e91dbeb18b034bbce366
probro91 <bozdener@gmail.com>
2023-02-04 17:45:27 -0500

    Adding firebase-config

---
+++ src/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBIGxyEgmbo9INgtqNRM5z3BaFapxhKWdI",
+    authDomain: "ratetrip-development.firebaseapp.com",
+    projectId: "ratetrip-development",
+    storageBucket: "ratetrip-development.appspot.com",
+    messagingSenderId: "234472668371",
+    appId: "1:234472668371:web:10778d6a5c74440bb7a826"
+  };
+
+  const app = initializeApp(firebaseConfig);
