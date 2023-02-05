4d76cb76b85d1d8af9742f7e2aba0f618fc0437d
katyatokarev <katyaltokarev>
2023-02-04 19:08:58 -0500

    Image slider

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -6,15 +6,33 @@ const ImageSlider = ({slides}) => {
 const [current, setCurrent] = useState(0)
 const length = slides.length
 
+const nextSlide = () => {
+    //images = length -1 (index)
+    setCurrent(current === length - 1 ? 0 : current + 1)
+}
+
+const prevSlide = () => {
+    setCurrent(current === 0 ? length -1 : current -1)
+}
 
 
+console.log(current)
+if(!Array.isArray(slides) || slides.length <= 0) {
+    return null
+}
   return (
     <section className="slider">
-        <FaArrowAltCircleLeft className="left-arrow"  />
-        <FaArrowAltCircleRight className="right-arrow"  />
+        <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
+        <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
     {SliderData.map((slide, index) => {
+
         return (
-            <img src={slide.image} alt="travel image" width="100" height="100"/>
+           <div className={index === current ? 'slide active' : 'slide'} key= 
+           {index}>
+            {index === current && (
+            <img src={slide.image} alt="travel image" className='image'/>)}
+               
+           </div> 
         )
      })}
     </section>
