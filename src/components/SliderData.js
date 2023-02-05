7010a2548d4dc42e8d8d9ad6d59e007d0059fdb0
katyatokarev <katyaltokarev>
2023-02-04 22:21:40 -0500

    Pics look better

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -3,7 +3,7 @@ export const SliderData = [
        image: 'https://images.unsplash.com/photo-1532931899774-fbd4de0008fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjh8fHJvYWQlMjB0cmlwfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1600&q=60'
     },
     {
-       image: 'https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cm9hZCUyMHRyaXB8ZW58MHx8MHx8&auto=format&fit=crop&w=1600&q=60'
+       image: 'https://images.unsplash.com/photo-1578158335529-27b8d78cfea5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHJvYWQlMjB0cmlwfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1600&q=60'
     },
     {
        image: 'https://images.unsplash.com/photo-1546489800-3f420ef6dca8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHJvYWQlMjB0cmlwfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1600&q=60'
