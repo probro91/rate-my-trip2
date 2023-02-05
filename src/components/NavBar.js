25afcb3e6fc779615fdc6c0dadfe061b3aaf7899
Ethan Ogle <encogle@gmail.com>
2023-02-04 21:38:31 -0500

    fixing NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,17 @@
 import PropTypes from "prop-types"
-//import Button from "./Button"
+
 import {Link, useMatch, useResolvedPath} from "react-router-dom"
 const NavBar = () => {
   return (
     <nav className = "nav">
       <Link to = "/" className = "site_title"> RateMyTrip</Link>
       <ul>
-       <CustomLink to = "/search">Search</CustomLink>
        <CustomLink to = "/explore">Explore</CustomLink>
        <CustomLink to ="/Posting">Post</CustomLink>
        <CustomLink to = "/signIn">Sign In</CustomLink>
        <CustomLink to = "/signUp">Sign Up</CustomLink>
       </ul>
     </nav>
-
-    // <div>
-    //   <h1>
-    //     {title}
-    //     <Button text="Search"/>
-    //     <Button text="Explore"/>
-    //     <Button text="Post"/>
-    //     <Button color="grey" text="Sign In"/>
-    //     <Button color="grey" text="Sign Out"/>
-    //   </h1>
-    // </div>
   )
 }
 
@@ -39,9 +27,6 @@ function CustomLink({to, children, ...props}) {
   )
 }
    
-
-
-
 NavBar.defaultProps = {
   title: "Navigation Bar"
 }
