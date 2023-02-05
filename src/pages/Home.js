908d584a8b5501d29bc2033d7d7ae95220f5a079
Ethan Ogle <encogle@gmail.com>
2023-02-05 05:02:31 -0500

    ghkv

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,31 @@
 import ImageSlider from "../components/ImageSlider"
 import { SliderData } from '../components/SliderData'
 import Post from "../components/Post"
+import {useState} from "react"
 
 
 const Home = () => {
+  const [search, setSearch] = useState("")
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    if(!search) {
+      alert("Please search for a trip!")
+      return
+    }
+
+    setSearch('')
+  }
   return (
-    <div>
-      <ImageSlider slides={SliderData}/>
+    <div className="parent">
+      <ImageSlider className="child" slides={SliderData}/>
+      <form className="child" onSubmit={onSubmit}>
+        <div className="txt_field">
+          <input type="text" placeholder="What is your next trip?" value={search} onChange={(e) => {setSearch(e.target.value)}}/>
+        </div>
+        <input type="submit" value="submit"/>
+      </form>
     </div>
   )
 }
