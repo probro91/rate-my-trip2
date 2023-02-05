eff09512ceb997613cc5544ec5d0e9b5337eb132
katyatokarev <katyaltokarev>
2023-02-05 02:06:21 -0500

    Tried CHS

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -6,9 +6,20 @@ export const SliderData = [
        image: 'https://images.unsplash.com/photo-1578158335529-27b8d78cfea5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHJvYWQlMjB0cmlwfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1600&q=60'
     },
     {
-       image: 'https://images.unsplash.com/photo-1546489800-3f420ef6dca8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHJvYWQlMjB0cmlwfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1600&q=60'
-    }
-    
+        image: 'https://media.cntraveler.com/photos/5cc23e267a570ff0cdde82c6/master/w_4000,h_2667,c_limit/BAHIA_EBFR4G.jpg'
+    },
+    {
+        image: 'https://a.cdn-hotels.com/gdcs/production165/d1089/b6515a53-6967-442d-8979-68c51f29a73b.jpg'
+    },
+    {
+        image: 'https://www.travelalaska.com/sites/default/files/2022-01/Haida-GlacierBay-GettyImages-1147753605.jpg'
+    },
+    {
+        image: 'https://afar.brightspotcdn.com/dims4/default/9ffda80/2147483647/strip/true/crop/1440x764+0+0/resize/1440x764!/quality/90/?url=https%3A%2F%2Fafar-media-production-web.s3.amazonaws.com%2Fbrightspot%2F0a%2Ff4%2Feedd84754c44a811c61c7d36cb43%2Fcharleston-shutterstock-2071518224.jpg'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1546489800-3f420ef6dca8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHJvYWQlMjB0cmlwfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1600&q=60'
+     },
     
     ]
     
