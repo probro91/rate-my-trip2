49c632345ec8048116c6f550cf1da4145c57e191
probro91 <bozdener@gmail.com>
2023-02-04 23:50:34 -0500

    Making navbar dynamic

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,54 @@
+import React, {useState} from "react"
 import PropTypes from "prop-types"
 import {Link, useMatch, useResolvedPath} from "react-router-dom"
 import {onAuthStateChanged, getAuth} from "firebase/auth"
+import {signOut} from "firebase/auth"
 
 const NavBar = () => {
-  const auth = getAuth()
-  let user = auth.onAuthStateChanged;
+  const logout = async () => {
+    await signOut(auth)
+  }
+  
+  const SignedInLinks = () => {
+    return (
+      <ul>
+        <Link to = "/" className = "site_title"> RateMyTrip</Link>
+        <CustomLink to = "/explore">Explore</CustomLink>
+        <CustomLink to ="/Posting">Post</CustomLink>
+        <a href = "/" onClick = {logout}>Sign Out</a>
+      </ul>
+    )
+  }
+  
+  const SignedOutLinks = () => {
+    return (
+      <ul>
+        <Link to = "/" className = "site_title"> RateMyTrip</Link>
+        <CustomLink to = "/explore">Explore</CustomLink>
+        <CustomLink to = "/signIn">Sign In</CustomLink>
+        <CustomLink to = "/signUp">Sign Up</CustomLink>
+      </ul>
+    )
+  }
+  const [links, setLinks] = useState(null);
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setLinks(SignedInLinks());
+    } else {
+      setLinks(SignedOutLinks());
+    }
+  });
 
   return (
     <nav className = "nav">
-      <Link to = "/" className = "site_title"> RateMyTrip</Link>
-      <ul>
-       <CustomLink to = "/explore">Explore</CustomLink>
-       {user && <CustomLink to ="/Posting">Post</CustomLink>}
-       {!user && <CustomLink to = "/signIn">Sign In</CustomLink>}
-       {!user &&<CustomLink to = "/signUp">Sign Up</CustomLink>}
-       {user &&<CustomLink to = "/signOut">Sign Out</CustomLink>}
-      </ul>
+      {links}
     </nav> 
   )
 }
+
+
+
 
 function CustomLink({to, children, ...props}) {
   const resolvedPath = useResolvedPath(to)
