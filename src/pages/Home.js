ad3fc867e6ac6a0a3d5b6b6bb454974bc5c705e5
Ethan Ogle <encogle@gmail.com>
2023-02-05 05:13:13 -0500

    hbj

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,14 +18,14 @@ const Home = () => {
     setSearch('')
   }
   return (
-    <div className="parent">
-      <ImageSlider className="child" slides={SliderData}/>
-      <form className="child" onSubmit={onSubmit}>
+    <div>
+      <form onSubmit={onSubmit}>
         <div className="txt_field">
-          <input type="text" placeholder="What is your next trip?" value={search} onChange={(e) => {setSearch(e.target.value)}}/>
+          <input type="text" placeholder="What's your next trip?" value={search} onChange={(e) => {setSearch(e.target.value)}}/>
         </div>
         <input type="submit" value="submit"/>
       </form>
+      <ImageSlider slides={SliderData}/>
     </div>
   )
 }
